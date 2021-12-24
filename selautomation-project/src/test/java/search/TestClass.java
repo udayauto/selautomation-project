c54f88a80284c7ae82027f70e82a16283c666cb2
udayk <udayk@DESKTOP-UJFK23I>
2021-12-24 12:47:05 +0530

    Test

---
+++ selautomation-project/src/test/java/search/TestClass.java
@@ -0,0 +1,13 @@
+package search;
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("GitHUb + Ecipse Integration");
+		
+		//Test commit
+		
+	}
+
+}
